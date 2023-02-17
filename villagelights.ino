993748fd5d68f42448544f7feb799ff378e620f1
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-02-17 11:46:50 +0000

    recieves simple messages and changes lights based on them

--- villagelights.ino
+++ villagelights.ino
@@ -1,53 +1,88 @@
+// In this version connect the control pin of the serial board directly to ground. Using a pin to select wasn't working for some reason and is frankly not important
+
 #include <FastLED.h>
 
 #define LED_PIN 6
-#define COLOR_ORDER RGB
-#define CHIPSET WS2811
-#define NUM_LEDS 100
+#define COLOR_ORDER GRB // GRB for WS2812, RGB for WS2811
+#define CHIPSET WS2812 // remember to set this for the correct lights
+#define NUM_LEDS 12
 
-#define BRIGHTNESS 255
+#define BRIGHTNESS 50 // 0-255
 #define FRAMES_PER_SECOND 60
+
+const unsigned int MAX_MESSAGE_LENGTH = 12;
 
 CRGB leds[NUM_LEDS];
 
-int incomingByte = 0; // for incoming serial data
 CRGB red = CRGB::Red;
 CRGB green = CRGB::Green;
 CRGB blue = CRGB::Blue;
 CRGB ledColour = red;
 
 void setup() {
-    delay(3000);
-    Serial.begin(57600); 
+    Serial.begin(9600); 
+    Serial.println("started");
     FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness( BRIGHTNESS );
 }
 
 
-void loop()
-{
+void loop(){
 
-    if (Serial.available() > 0) {
-        // read the incoming byte:
-        incomingByte = Serial.read();
+    //Check to see if anything is available in the serial receive buffer
+    while (Serial.available() > 0)
+    {
+    //Create a place to hold the incoming message
+    static char message[MAX_MESSAGE_LENGTH];
+    static unsigned int message_pos = 0;
 
-        // prints the received data
-        Serial.print("I received: ");
-        Serial.println(incomingByte);
+    //Read the next available byte in the serial receive buffer
+    char inByte = Serial.read();
+
+    //Message coming in (check not terminating character) and guard for over message size
+    if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) )
+    {
+        //Add the incoming byte to our message
+        message[message_pos] = inByte;
+        message_pos++;
     }
+    //Full message received...
+    else
+    {
+        //Add null character to string
+        message[message_pos] = '\0';
 
-    if (incomingByte == 103) {
-        ledColour = green;
-    } else if (incomingByte == 114) {
-        ledColour = red;
-    } else if (incomingByte == 98) {
-        ledColour = blue;
-    }
+        //Print the message (or do other things)
 
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ledColour;
+        Serial.println(message);
+
+        int lightControl = atoi(message);
+
+        showLights(lightControl);
+
+
+        //Reset for the next message
+        message_pos = 0;
     }
-    
-    FastLED.show(); // display this frame
-    FastLED.delay(1000 / FRAMES_PER_SECOND);
+    }
 }
+
+void showLights(int lightControl){
+    Serial.println("banana");
+    Serial.println(lightControl);
+    if (lightControl == 0) {
+            ledColour = green;
+        } else if (lightControl == 1) {
+            ledColour = red;
+        } else if (lightControl == 98) {
+            ledColour = blue;
+        }
+
+        for( int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = ledColour;
+        }
+        
+        FastLED.show(); // display this frame
+        FastLED.delay(1000 / FRAMES_PER_SECOND);
+}
+
